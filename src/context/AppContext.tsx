b77c27569b2fad417d6fe3716c155e7396c5fe07
Viral Patel <pviral3011@gmail.com>
2025-03-23 10:53:30 +0530

    feat: fetched doctors list from api

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -1,5 +1,7 @@
-import { createContext } from "react";
-import { AppContextType, doctors } from "../assets/assets";
+import { createContext, useEffect, useState } from "react";
+import { AppContextType } from "../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const defaultValue: AppContextType = {
   doctors: [], // Default to an empty array if no doctors are available.
@@ -10,10 +12,33 @@ export const AppContext = createContext<AppContextType>(defaultValue);
 
 const AppContextProvider = (props: any) => {
   const currencySymbol = "$";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const [doctors, setDoctors] = useState([]);
+
   const value = {
     doctors,
     currencySymbol,
   };
+
+  const getDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+
+      if (data.success) {
+        setDoctors(data.doctors);
+      } else {
+        toast.error("Error fetching doctors");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Error fetching doctors");
+    }
+  };
+
+  useEffect(() => {
+    getDoctorsData();
+  }, []);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
